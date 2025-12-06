617a05a594b088d72c41dc7e519705b3c9432805
Souvik Dutta <38257990+SkD18@users.noreply.github.com>
2025-12-06 21:17:41 +0530

    assignment - W1

--- DS/Multiply two strings.java
+++ DS/Multiply two strings.java
@@ -1,6 +1,8 @@
 class Solution {
     public String multiplyStrings(String s1, String s2) {
         int sign = 1;
+
+        // Handle negative signs
         if (s1.charAt(0) == '-') {
             sign *= -1;
             s1 = s1.substring(1);
@@ -10,6 +12,7 @@ class Solution {
             s2 = s2.substring(1);
         }
 
+        // Remove leading zeros
         while (s1.length() > 1 && s1.charAt(0) == '0') {
             s1 = s1.substring(1);
         }
@@ -17,6 +20,7 @@ class Solution {
             s2 = s2.substring(1);
         }
 
+        // If any string becomes "0" after trimming
         if (s1.equals("0") || s2.equals("0")) {
             return "0";
         }
@@ -24,19 +28,23 @@ class Solution {
         int n = s1.length(), m = s2.length();
         int[] res = new int[n + m];
 
+        // Multiply each digit
         for (int i = n - 1; i >= 0; i--) {
             for (int j = m - 1; j >= 0; j--) {
                 res[i + j + 1] += (s1.charAt(i) - '0') * (s2.charAt(j) - '0');
             }
         }
 
+        // Handle carry over
         for (int i = n + m - 1; i > 0; i--) {
             res[i - 1] += res[i] / 10;
             res[i] %= 10;
         }
 
+        // Skip leading zero if present
         int idx = (res[0] == 0) ? 1 : 0;
 
+        // Build result string
         StringBuilder ans = new StringBuilder();
         for (; idx < n + m; idx++) {
             ans.append(res[idx]);
@@ -46,7 +54,6 @@ class Solution {
             ans.insert(0, '-');
         }
 
-        return ans+"";
-
+        return ans.toString();
     }
 }
